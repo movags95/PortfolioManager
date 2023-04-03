9f0f37035a81968177c36f2d1a44d019af664b03
movags95 <45894549+movags95@users.noreply.github.com>
2023-04-03 23:53:24 +0100

    Added function to Asset class to insert into  asset type

--- app/models.py
+++ app/models.py
@@ -10,15 +10,18 @@ class Asset:
 
     # Closes the connection when object is destroyed
     def __del__(self):
+        self.conn.commit()
         self.conn.close()
-    
+        
     def add_asset_type(self):
-        self.cursor.execute("INSERT INTO asset_type (type) VALUES (?)", self.type)
+        self.cursor.execute("INSERT INTO asset_types (type) VALUES (?)", (self.type,))
+        
         
 
     
 
 if __name__ == '__main__':
     asset = Asset(symbol='AAPL', type='Stock')
+    asset.add_asset_type()
     print(asset.symbol)
     print(asset.type)
