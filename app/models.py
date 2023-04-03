19aa2eb318ef905f4f9469ebd1c1ed5780662207
movags95 <45894549+movags95@users.noreply.github.com>
2023-04-03 23:42:35 +0100

    Added models.py to store classes for assets

---
+++ app/models.py
@@ -0,0 +1,24 @@
+from config import DB_PATH
+import sqlite3
+
+class Asset:
+    def __init__(self, symbol, type, path=DB_PATH):
+        self.symbol = symbol
+        self.type = type
+        self.conn = sqlite3.connect(path)
+        self.cursor = self.conn.cursor()
+
+    # Closes the connection when object is destroyed
+    def __del__(self):
+        self.conn.close()
+    
+    def add_asset_type(self):
+        self.cursor.execute("INSERT INTO asset_type (type) VALUES (?)", self.type)
+        
+
+    
+
+if __name__ == '__main__':
+    asset = Asset(symbol='AAPL', type='Stock')
+    print(asset.symbol)
+    print(asset.type)
