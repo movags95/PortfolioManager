db3063440bee55c38d8dbee4682340e5f66451d8
movags95 <45894549+movags95@users.noreply.github.com>
2023-04-03 21:36:24 +0100

    Simplified the install script. Commented out virtualenv and requirement functions. This should be implemented as a bash script.

--- run.py
+++
@@ -1,9 +0,0 @@
-from app.install import create_db, configure_environment, create_requirements_txt, reset_environment, delete_db
-
-
-if __name__ == '__main__':
-    # reset_environment()
-    # create_requirements_txt()
-    delete_db()
-    create_db()
-    configure_environment()
