d6079935c2f0285845cf5cc976fe22122695a55c
movags95 <45894549+movags95@users.noreply.github.com>
2023-04-05 00:53:59 +0100

    Modified db helpers for ease of use.

--- app/helpers/db_helpers.py
+++ app/helpers/db_helpers.py
@@ -1,78 +1,68 @@
-import sqlite3
-from config import DB_PATH
-conn = sqlite3.connect(DB_PATH)
+def get_asset_types(cursor):
+    cursor.execute('SELECT * FROM asset_types')
+    return cursor.fetchall()
 
-def validate_connection(db_conn=conn):
-    if not isinstance(db_conn, sqlite3.Connection):
-        raise TypeError("Input must be a sqlite3.Connection object")
+def get_assets(cursor):
+    cursor.execute('SELECT * FROM assets')
+    return cursor.fetchall()
 
-validate_connection(conn)
+def get_transactions(cursor):
+    cursor.execute('SELECT * FROM transactions')
+    return cursor.fetchall()
 
-def execute(sql_stmt, values=None, db_conn=conn):
-    c = db_conn.cursor()
-    if values is None:
-        c.execute(sql_stmt)
-    else:
-        c.execute(sql_stmt, values)
-
-    return c.fetchall()
-
-def commit(db_conn=conn):
-    db_conn.commit()
-    db_conn.close()
-
-
-
-
-
-
-def get_asset_types():
-    return execute('SELECT * FROM asset_types')
-
-def get_assets():
-    return execute('SELECT * FROM assets')
-
-def get_transactions():
-    return execute('SELECT * FROM transactions')
-
-def get_asset_type_id_for(type):
+def get_asset_type_id_for(cursor, type):
     type = type.capitalize()
-    types = get_asset_types()
+    types = get_asset_types(cursor)
     for row in types:
         if type in row:
             return row[0]
 
-def get_asset_id_for(symbol):
-    symbol = symbol.capitalize()
-    assets = get_assets()
-    for row in assets:
-        if symbol in row:
-            return row[0]
+def get_asset_id_for(cursor, symbol=None, asset_type_id=None):
+    # This will run when the function is supplied with an asset_type_id and will return a tuple of values
+    if asset_type_id != None:
+        asset_ids = []
+        assets = get_assets(c)
+        for row in assets:
+            if asset_type_id == row[3]:
+                asset_ids.append(row[0])
+        return asset_ids
 
-def insert_into_asset_types(asset_type):
+    # This will run when the function is supplied with a symbol and can only return one value
+    if symbol != None:
+        symbol = symbol.upper()
+        assets = get_assets(c)
+        for row in assets:
+            if symbol in row:
+                return row[0]
+    
+
+def insert_into_asset_types(cursor, asset_type):
     asset_type = asset_type.capitalize()
-    execute('INSERT INTO asset_types (type) VALUES (?)', (asset_type,))
-    commit()
+    cursor.execute('INSERT INTO asset_types (type) VALUES (?)', (asset_type,))
 
-def insert_into_assets(symbol, asset_type):
+
+def insert_into_assets(cursor, symbol, asset_type):
     symbol = symbol.upper()
     asset_type = asset_type.capitalize()
-    asset_type_id = get_asset_type_id_for(asset_type)
-    execute('INSERT INTO assets (symbol, asset_type_id) VALUES (?,?)', (symbol, asset_type_id,))
-    commit()
+    asset_type_id = get_asset_type_id_for(cursor, asset_type)
+    cursor.execute('INSERT INTO assets (symbol, asset_type_id) VALUES (?,?)', (symbol, asset_type_id,))
 
-def insert_into_transactions(symbol, price, qty, currency='GBP'):
+
+def insert_into_transactions(cursor, symbol, price, qty, currency='GBP'):
     symbol = symbol.upper()
     currency = currency.upper()
-    asset_id = get_asset_id_for(symbol)
-    execute('INSERT INTO transactions (asset_id, purchase_currency, purchase_price, quantity) VALUES (?,?,?,?)',
+    asset_id = get_asset_id_for(cursor, symbol)
+    cursor.execute('INSERT INTO transactions (asset_id, purchase_currency, purchase_price, quantity) VALUES (?,?,?,?)',
             (asset_id, currency, price, qty,)
             )
-    commit()
+
+def update_asset_current_price(cursor, symbol, current_price):
+    symbol = symbol.upper()
+    asset_id = get_asset_id_for(cursor, symbol)
+    cursor.execute('''
+    UPDATE assets SET current_price = ? WHERE asset_id = ?
+    ''', (current_price, asset_id))
 
 
 
-
-
-    
 
